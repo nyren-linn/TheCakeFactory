c28ebaa2948f775ef3bca8c08c3d945c539d0725
Linn <nyren.linn@gmail.com>
2021-01-15 14:09:53 +0100

    La till några dependencies i build.gradle samt la till en src-mapp för körning av spring boot applicationen.

---
+++ src/main/java/com/project/TheCakeFactory/TheCakeFactoryApplication.java
@@ -0,0 +1,22 @@
+package com.project.TheCakeFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TheCakeFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCakeFactoryApplication.class, args);
+	}
+
+	@GetMapping("/welcome")
+	public String welcome(@RequestParam(value = "name", defaultValue = "sugar") String name) {
+		return String.format("Welcome %s!", name);
+	}
+
+}
---
+++ src/test/java/com/project/TheCakeFactory/TheCakeFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.TheCakeFactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCakeFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
